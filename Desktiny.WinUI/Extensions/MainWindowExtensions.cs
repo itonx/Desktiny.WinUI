6e8f1ed8d405a214d8a94d7e8e2f7bbe511e767b
Ayrton Teniente <ayrton.94.e@gmail.com>
2026-08-16 00:54:20 -0600

    Add missing 'this' keyword

--- Desktiny.WinUI/Extensions/MainWindowExtensions.cs
+++ Desktiny.WinUI/Extensions/MainWindowExtensions.cs
@@ -11,32 +11,32 @@ namespace Desktiny.WinUI.Extensions
 {
     public static class MainWindowExtensions
     {
-        public static nint GetWindowHandle(Window window)
+        public static nint GetWindowHandle(this Window window)
         {
             return Tools.WindowHelper.GetWindowHandle(window);
         }
 
-        public static WindowId GetWindowId(Window window)
+        public static WindowId GetWindowId(this Window window)
         {
             return Tools.WindowHelper.GetWindowId(window);
         }
 
-        public static AppWindow GetAppWindow(Window window)
+        public static AppWindow GetAppWindow(this Window window)
         {
             return Tools.WindowHelper.GetAppWindow(window);
         }
 
-        public static void HideAppWindow(Window window)
+        public static void HideAppWindow(this Window window)
         {
             Tools.WindowHelper.HideAppWindow(window);
         }
 
-        public static void MinimizeAppWindow(Window window)
+        public static void MinimizeAppWindow(this Window window)
         {
             Tools.WindowHelper.MinimizeAppWindow(window);
         }
 
-        public static async Task SetWindowPositionToCenter(Window window)
+        public static async Task SetWindowPositionToCenter(this Window window)
         {
             await Tools.WindowHelper.SetWindowPositionToCenter(window);
         }
